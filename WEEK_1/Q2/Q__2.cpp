99328314c3bc6c6cc4d536222c6636d2e0a5c148
jitesh <jiteshjitsun@gmail.com>
2021-07-31 09:28:09 +0530

    BinaryS

--- WEEK_1/Q2/Q__2.cpp
+++ WEEK_1/Q2/Q__2.cpp
@@ -6,7 +6,7 @@ int main()
 {
     freopen("input.txt", "r", stdin);   //input to be taken from input.txt
     freopen("output.txt", "w", stdout); //output to be printed in output.txt
-    int t, n, k, ele,l,c=0;
+    int t, n, k, ele,c=0;
     vector<int> v1;
     cin >> t;
     for(int i=0; i<t; i++)
@@ -18,10 +18,29 @@ int main()
             v1.push_back(ele);
         }
         cin >> k;
-        if(k>)
-        if (c==0)
-            cout << "Not Present " << v1.size() << endl;
-        c=0;
+        
+        int l=0,h=n-1,mid;
+        while(l<=h)
+        {
+            mid = (l+h)/2;
+            if(k==v1[mid])
+            {
+                cout << "present "<< mid << endl;
+                c = 1;
+                break;
+            }
+            else if(k>v1[mid])
+            {
+                l = mid+1;
+            }
+            else 
+            {
+                h = mid-1;
+            }
+        }
+        if(c==0)
+            cout << "Not present "<< n << endl;
+        c = 0;
         v1.clear();
 
     }
