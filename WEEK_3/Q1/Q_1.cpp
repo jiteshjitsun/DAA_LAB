c299de0af58fbf0f93c4b89fd4128210438edbd1
jitesh <jiteshjitsun@gmail.com>
2021-08-05 12:28:42 +0530

    insertion sort

---
+++ WEEK_3/Q1/Q_1.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int t,n, comp = 0, shift = 0;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        int arr[n];
+        for(int i=0; i<n; i++)
+        {
+            cin >> arr[i];
+        }
+        for(int i=1; i<n; i++)
+        {
+            int temp = arr[i];
+            int j= i-1;
+            while(j>-1 && temp<arr[j])
+            {
+                arr[j+1] = arr[j];
+                j-=1;
+                comp +=1;
+            }
+            arr[j+1] = temp;
+            shift +=1;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        cout << "comparisons" << " = " << comp << endl;
+        cout << "shifts" << " = " << shift + comp << endl;
+        comp = 0;
+        shift = 0;
+    }
+}
